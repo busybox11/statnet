80521d0af2420c659cc68a67ecdc9b7dbe808144
busybox11 <planetgeek11@gmail.com>
2020-11-03 23:30:53 +0100

    Actually working stats page

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from __init__ import app
+import os
+app._static_folder = os.path.abspath("static/")
+
+from flask import Flask, request, send_from_directory
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
 
 @app.route("/")
 def root():
-    return app.send_static_file('stats/stats.html')
+    root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(os.path.join(root_dir, 'static'), 'stats.html')
