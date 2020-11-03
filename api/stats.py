11a68bf8313b0a118184370399e8f61634b8c8ab
busybox11 <planetgeek11@gmail.com>
2020-11-04 00:06:02 +0100

    Comments, yay

--- api/stats.py
+++ api/stats.py
@@ -17,6 +17,7 @@ class processClass:
         p.start()
 
     def run(self):
+        # If the interface is "lo", skip it
         if list(psutil.net_io_counters(pernic=True).keys())[0] == "lo":
             interface = list(psutil.net_io_counters(pernic=True).keys())[1]
         else:
@@ -28,18 +29,23 @@ class processClass:
             up[0] = stats.bytes_sent
             down[1] = down[0]
             down[0] = stats.bytes_recv
+            # Saves the stats data to a pickle file 
             with open('stats.pickle', 'wb') as f:
                 pickle.dump([interface, up, down], f, protocol=pickle.HIGHEST_PROTOCOL)
+
+            # Updating every second
             time.sleep(1)
 
     def get_stats(self):
         print(interface)
         return up
 
+# Run stats fetch as background task
 stats_class = processClass()
 
 @api.route("/stats")
 def stats():
+    # Retrieves the stats data from a pickle file
     with open('stats.pickle', 'rb') as f:
         stats = pickle.load(f)
         global interface, up, down
@@ -47,4 +53,5 @@ def stats():
         up = stats[1]
         down = stats[2]
     
+    # Sends the stats as a JSON object
     return jsonify({"interface": interface, "up": up[0] - up[1], "down": down[0] - down[1]})
